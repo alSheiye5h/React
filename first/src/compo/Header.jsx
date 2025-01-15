943516eabcb495a65efc5e9421649390faa90b4d
alSheiye5h <anasbelkhadir13@gmail.com>
2025-01-15 15:24:38 +0100

    _

---
+++ first/src/compo/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <header>
+            <p>hello world</p>
+            <ul>navBar :
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header
