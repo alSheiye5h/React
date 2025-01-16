3501db7796b444cafea4b409bbce8202af7212a6
alSheiye5h <anasbelkhadir13@gmail.com>
2025-01-16 17:15:17 +0100

    _

--- second/src/components/YesNoButtons.jsx
+++ second/src/components/YesNoButtons.jsx
@@ -2,10 +2,21 @@
 
 
 function YesNoButtons() {
+
+    let count = 1;
+
+    let handleNo = () => {
+        return count < 3 ? alert(`user refused ${count++} !`) : (alert(`moi j suis enought ${count} !`), count = 1) ;   
+    }
+
+    let handleYes = () => {
+        alert("user accepted !");
+    }
+
     return (
         <>
-        <button className="yesButtons">Good</button>        
-        <button className="NoButtons">Bad</button>        
+        <button className="yesButtons" onClick={handleYes} >Good</button>        
+        <button className="NoButtons" onClick={handleNo} >Bad</button>        
         </>
     )
 }
