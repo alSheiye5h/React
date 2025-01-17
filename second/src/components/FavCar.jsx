5fdeb2cdb919ce8f016c673711aa45429b44f05c
alSheiye5h <anasbelkhadir13@gmail.com>
2025-01-17 19:35:23 +0100

    _

--- second/src/components/FavCar.jsx
+++ second/src/components/FavCar.jsx
@@ -2,30 +2,6 @@ import { useState } from "react";
 
 function FavCar() {
 
-    // let [car, setCar] = useState({
-    //     year: 2024,
-    //     model: "mercedes G63",
-    //     color: "black",
-    // });
-
-
-    // let changeCar = (event, t) => {
-    //     if (t == 1) 
-    //         setCar(car => ({...car ,model: event.target.value}))
-    //     else if (t == 2)
-    //         setCar(car => ({...car ,color: event.target.value}))
-    //     else
-    //         setCar(car => ({...car ,year: event.target.value}))
-    // } 
-
-    // return (
-    //     <div className="window" style={{width: "fit-content"}}>
-    //         <p>Your Favourite Car is a {car.color} {car.model} of year {car.year}</p>
-    //         <input type="text" value={car.model} className="carInp" onChange={(event) => {changeCar(event,1)}} />
-    //         <input type="text" value={car.color} className="carInp" onChange={(event) => {changeCar(event,2)}} />
-    //         <input type="text" value={car.year} className="carInp" onChange={(event) => {changeCar(event,3)}} />
-    //     </div>
-    // )
     let [car, setCar] = useState({
         year: 2024,
         model: "mercedes G63",
