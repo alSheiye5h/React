fc207e3a5a7053d8db40097d6dfb65e5aaabd029
alSheiye5h <anasbelkhadir13@gmail.com>
2025-01-17 20:49:43 +0100

    _

--- second/src/components/FoodList.jsx
+++ second/src/components/FoodList.jsx
@@ -6,14 +6,18 @@ function FoodList() {
     let [foods, setFoods] = useState(["apple", "banana", "orange"])
     let [food, setFood] = useState("")
 
-    let foodsHtml = foods.map(e => <li key={e} >{e}</li>)
+    let foodsHtml = foods.map((e, index) => <li key={index} >{e}</li>)
 
     let foodInpChange = (event) => {
         setFood(event.target.value)
     }
 
-    let addFood = (event) => {
-        setFoods(f => f.push(food))
+    let addFood = () => {
+        if (food.trim("") !== "") {
+            setFoods(f => [...f, food]);
+            setFood("");
+        }
+
     }
 
 
@@ -23,8 +27,9 @@ function FoodList() {
             <ul>
                 {foodsHtml}
             </ul>
-            <input type="text" className="" value={food} onChange={foodInpChange}/>
-            <button className="" onClick={addFood} >add food</button>
+            <input type="text" className="finp" value={food} onChange={foodInpChange}/>
+            <br />
+            <button className="fbtn" onClick={addFood} >add food</button>
         </div>
     )
 }
