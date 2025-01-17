37129a7b43072bc533742dd1199cc758511c363c
alSheiye5h <anasbelkhadir13@gmail.com>
2025-01-17 19:35:10 +0100

    _

--- second/src/components/FavCar.jsx
+++ second/src/components/FavCar.jsx
@@ -1,27 +1,75 @@
-
+import { useState } from "react";
 
 function FavCar() {
 
+    // let [car, setCar] = useState({
+    //     year: 2024,
+    //     model: "mercedes G63",
+    //     color: "black",
+    // });
+
+
+    // let changeCar = (event, t) => {
+    //     if (t == 1) 
+    //         setCar(car => ({...car ,model: event.target.value}))
+    //     else if (t == 2)
+    //         setCar(car => ({...car ,color: event.target.value}))
+    //     else
+    //         setCar(car => ({...car ,year: event.target.value}))
+    // } 
+
+    // return (
+    //     <div className="window" style={{width: "fit-content"}}>
+    //         <p>Your Favourite Car is a {car.color} {car.model} of year {car.year}</p>
+    //         <input type="text" value={car.model} className="carInp" onChange={(event) => {changeCar(event,1)}} />
+    //         <input type="text" value={car.color} className="carInp" onChange={(event) => {changeCar(event,2)}} />
+    //         <input type="text" value={car.year} className="carInp" onChange={(event) => {changeCar(event,3)}} />
+    //     </div>
+    // )
     let [car, setCar] = useState({
         year: 2024,
         model: "mercedes G63",
         color: "black",
     });
 
+    function changeCarModel(event) {
+        setCar(c => ({...c, model: event.target.value}))
+    }
 
-    let changeCar = (event, t) => {
-        if (t == 1) 
-            setCar(car.model = event.target.value)
-    } 
+    function changeCarColor(event) {
+        setCar(c => ({...c, color: event.target.value}))
+    }
+
+    function changeCarYear(event) {
+        setCar(c => ({...c, year: event.target.year}))
+    }
 
     return (
-        <div className="window" style={{width: "fit-content"}}>
+        <div className="window" style={{ width: "fit-content" }}>
             <p>Your Favourite Car is a {car.color} {car.model} of year {car.year}</p>
-            <input type="text" className="carInp" onChange={(event) => {changeCar(event,1)}} />
-            <input type="text" className="carInp" onChange={(event) => {changeCar(event,2)}} />
-            <input type="text" className="carInp" onChange={(event) => {changeCar(event,3)}} />
+            <input 
+                type="text" 
+                value={car.model} 
+                className="carInp" 
+                onChange={changeCarModel} 
+                placeholder="Model"
+            />
+            <input 
+                type="text" 
+                value={car.color} 
+                className="carInp" 
+                onChange={changeCarColor} 
+                placeholder="Color"
+            />
+            <input 
+                type="number"
+                value={car.year}
+                className="carInp"
+                onChange={changeCarYear} 
+                placeholder="Year"
+            />
         </div>
-    )
+    );
 }
 
 export default FavCar
