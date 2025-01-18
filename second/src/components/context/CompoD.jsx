7b4e01a3ddecbdd8b2487fdbce4176ec1c417797
alSheiye5h <anasbelkhadir13@gmail.com>
2025-01-18 21:18:15 +0100

    _

--- second/src/components/context/CompoD.jsx
+++ second/src/components/context/CompoD.jsx
@@ -1,18 +1,25 @@
 import { useState, useContext, createContext } from "react";
-
+import { MessageContext } from './CompoA'
 
 
 function CompoD() {
 
     const setMessage = useContext(MessageContext);
 
+    const [localMessage, setLocalMessage] = useState(""); 
 
-    
+    function localMessageInp(event) {
+        setLocalMessage(event.target.value)
+    }
+
+    function sendMessage() {
+        setMessage(localMessage)
+    }
 
     return (
         <div className="compoX compod" style={{flexDirection: "column", gap: "5px"}}>
-            <input type="text" value={message} onChange={Message} />        
-            <button>send</button>
+            <input type="text" value={localMessage} onChange={localMessageInp} />        
+            <button onClick={sendMessage} >send</button>
         </div>
     )
 }
