a1b7cfcb0f29bbcda5c6803f46d3f27ce171479c
alSheiye5h <anasbelkhadir13@gmail.com>
2025-01-18 17:46:11 +0100

    _

--- second/src/components/DigitalClock.jsx
+++ second/src/components/DigitalClock.jsx
@@ -2,24 +2,26 @@ import { useState, useEffect } from "react"
 
 function DigitalClock() {
 
-    let [time, setTime] = useState();
+    let [time, setTime] = useState(new Date());
 
     useEffect(() => {
         let interval = setInterval(() => {
             setTime(new Date())
         }, 1000)
         
-        return clearInterval(interval);
-    }, interval);
+        return () => clearInterval(interval);
+    }, []);
+
+    const formatNumber = (num) => String(num).padStart(2, "0");
 
 
 
 
     return (
         <div className="clockDiv" >
-            <span>aa :</span>    
-            <span> aa :</span>    
-            <span> aa</span>    
+            <span>{formatNumber(time.getHours())} </span>:    
+            <span> {formatNumber(time.getMinutes())} </span>:   
+            <span> {formatNumber(time.getSeconds())}</span>    
 
         </div>
     )
