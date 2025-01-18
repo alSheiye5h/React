089efecd5ab0531c644456722e1a0cef42efe3c5
alSheiye5h <anasbelkhadir13@gmail.com>
2025-01-18 17:28:22 +0100

    _

--- second/src/components/DigitalClock.jsx
+++ second/src/components/DigitalClock.jsx
@@ -4,6 +4,13 @@ import { useState, useEffect } from "react"
 function DigitalClock() {
 
 
+
+    return (
+        <div className="clockDiv" >
+
+
+        </div>
+    )
 }
 
 export default DigitalClock
