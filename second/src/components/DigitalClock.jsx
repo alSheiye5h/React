85d582c95c8e4fd371196a8e159f688f3aba7d51
alSheiye5h <anasbelkhadir13@gmail.com>
2025-01-18 17:51:56 +0100

    _

--- second/src/components/DigitalClock.jsx
+++ second/src/components/DigitalClock.jsx
@@ -1,30 +1,97 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
+
 
 function DigitalClock() {
 
     let [time, setTime] = useState(new Date());
 
+
     useEffect(() => {
-        let interval = setInterval(() => {
-            setTime(new Date())
-        }, 1000)
-        
-        return () => clearInterval(interval);
-    }, []);
+        setTime(new Date())
 
-    const formatNumber = (num) => String(num).padStart(2, "0");
+        return () => {}
+    }, [])
 
-
-
+    let formatedDate = (time) => String(time).padStart(2, "0");
 
     return (
-        <div className="clockDiv" >
-            <span>{formatNumber(time.getHours())} </span>:    
-            <span> {formatNumber(time.getMinutes())} </span>:   
-            <span> {formatNumber(time.getSeconds())}</span>    
-
+        <div className="clockDiv">
+            <span></span>
+            <span></span>
+            <span></span>
         </div>
     )
+
 }
 
 export default DigitalClock
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState, useEffect } from "react"
+
+// function DigitalClock() {
+
+//     let [time, setTime] = useState(new Date());
+
+//     useEffect(() => {
+//         let interval = setInterval(() => {
+//             setTime(new Date())
+//         }, 1000)
+        
+//         return () => clearInterval(interval);
+//     }, []);
+
+//     const formatNumber = (num) => String(num).padStart(2, "0");
+
+
+
+
+//     return (
+//         <div className="clockDiv" >
+//             <span>{formatNumber(time.getHours())} </span>:    
+//             <span> {formatNumber(time.getMinutes())} </span>:   
+//             <span> {formatNumber(time.getSeconds())}</span>    
+
+//         </div>
+//     )
+// }
+
+// export default DigitalClock
