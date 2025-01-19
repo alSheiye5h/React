d1742a7c82a61f2e3dace9b3bd4d538c2215a2ad
alSheiye5h <anasbelkhadir13@gmail.com>
2025-01-19 13:55:12 +0100

    _

--- second/src/components/Ref.jsx
+++ second/src/components/Ref.jsx
@@ -1,11 +1,33 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 function Ref() {
 
+    let display1 = useRef("block");
+    let display2 = useRef("none");
+    let display3 = useRef("none");
+    let display4 = useRef("none");
+
+    let choose = () => {
+        let choosen = [display1, display2, display3, display4][Math.ceil(Math.random() * 4)];
+        
+        if (choosen.current == "none") {
+            [display1, display2, display3, display4].map(e => {
+                e.current == "none";
+            })
+            choosen.current == "block"
+        } else {
+            choose();
+        }
+    }
+
+    useEffect(choose, [display1, display2, display3, display4])
 
     return (
-        <div className="window" style={{height: "400px"}} >
-            <button className="buttonRef">click me</button>
+        <div className="window" style={{height: "400px", gap: "10px"}} >
+            <button className="buttonRef ref1" style={{display: display1.current}} onBlur={hide} >click me</button>
+            <button className="buttonRef ref2" style={{display: display2.current}} onBlur={hide}>click me</button>
+            <button className="buttonRef ref3" style={{display: display3.current}} onBlur={hide}>click me</button>
+            <button className="buttonRef ref4" style={{display: display4.current}} onBlur={hide}>click me</button>
         </div>
     )
 
